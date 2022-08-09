784854a34a688b25032bd77cdb01e7bdbf8115e4
Jenan <j.azzubaydi@gmail.com>
2022-08-09 14:31:46 +0100

    joinclean finished and added put function to logclean

--- routes/joinClean.js
+++ routes/joinClean.js
@@ -1,32 +1,36 @@
 import express from "express";
 const router = express.Router();
-import {
-  getJoinClean,
-  postJoinCleanup,
-  updEvent,
-} from "../models/joinClean.js";
+import {getJoinClean, postJoinCleanup,updEvent,deleteById} from "../models/joinClean.js"
+
 
 //Route to GET all entries in Log a clean table
 router.get("/", async function (req, res) {
-  const result = await getJoinClean();
-  res.status(200).json({ success: true, payload: result.rows });
+  const result = await getJoinClean()
+  res.status(200).json({success:true, payload: result.rows});
 });
 
-//Post
+//Post 
 router.post("/", async function (req, res) {
   const body = req.body;
-  const result = await postJoinCleanup(body);
-  // console.log(`new joiner posted: ${result}`);
-  res.status(200).json({ success: true, payload: result.rows });
+  const result = await postJoinCleanup(body)
+  console.log(`new joiner posted: ${result}`)
+  res.status(200).json({success:true, payload: result.rows});
 });
 
-//PUT
-router.put("/:id", async (req, res) => {
+
+//Put
+router.put('/id',async(req,res)=>{
   const id = Number(req.params.id);
   const data = req.body;
-  console.log(id, data);
-  const result = await updEvent(id, data);
-  res.json({ success: true, payload: result.rows });
+  const result = await updEvent(id, data)
+  res.json({success: true, payload: result.rows });
 });
+
+//Delete
+router.delete("/:id", async (req, res) => {
+   const id = req.params.id
+   const result = await deleteById(id)
+   res.json({success: true, payload: result.rows })
+  });
 
 export default router;
