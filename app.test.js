271515de8740ea93237c0a1e4c8875098652e637
Simran Rai <simran_rai982@outlook.com>
2022-08-09 16:26:08 +0100

    test route for get for all routes

--- app.test.js
+++ app.test.js
@@ -1,13 +1,34 @@
 import request from "supertest";
 import { jest } from "@jest/globals";
-import supertest from "supertest";
+// import supertest from "supertest";
 
 import app from "./app.js";
 
 jest.setTimeout(7000);
 
-describe("GET /joinclean", () => {
-  test("gives us back 200", async () => {
-    await request(app).get("/joinclean");
+//can write tests for GET, POST, PUT, DELETE
+describe("Tests for /joinclean route", () => {
+  test("GET /joinclean", async () => {
+    let res = await request(app).get("/joinclean");
+    expect(res.statusCode).toEqual(200);
+    // console.log(res._body);
+  });
+});
+
+//can write tests for GET, POST, PUT
+describe("Tests for /logclean route", () => {
+  test("GET /logclean", async () => {
+    let res = await request(app).get("/logclean");
+    expect(res.statusCode).toEqual(200);
+    // console.log(res._body);
+  });
+});
+
+//can write tests for GET, POST
+describe("Tests for /startclean route", () => {
+  test("GET /logclean", async () => {
+    let res = await request(app).get("/logclean");
+    expect(res.statusCode).toEqual(200);
+    console.log(res);
   });
 });
