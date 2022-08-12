6a5938ba12ca4963553597709fca3d8939c6091e
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-08-12 14:30:14 +0100

    force redeploy

--- models/joinClean.js
+++ models/joinClean.js
@@ -4,13 +4,12 @@ import { query } from "../db/connection.js";
 export async function getJoinClean() {
   const data = await query(`SELECT * FROM joinClean;`);
   console.log("This is the join clean table", data.rows);
-  return data
+  return data;
 }
-
 
 //Post
 export async function postJoinCleanup(body) {
-  const {name, comments} = body;
+  const { name, comments } = body;
   const data = await query(
     `INSERT INTO joinClean (
       name,
@@ -18,22 +17,21 @@ export async function postJoinCleanup(body) {
     ) VALUES ($1, $2 ) RETURNING *;`,
     [name, comments]
   );
-  return (data)
+  return data;
 }
 
 //PUT
-export async function updEvent(id, data){
-  const sqlString = `UPDATE joinClean SET name = $1 , comments = $2 WHERE id = $3 RETURNING *;`
-  const result = await query(sqlString,[data.name, data.comments, id]); 
-  return result
+export async function updEvent(id, data) {
+  const sqlString = `UPDATE joinClean SET name = $1 , comments = $2 WHERE id = $3 RETURNING *;`;
+  const result = await query(sqlString, [data.name, data.comments, id]);
+  return result;
 }
 
 //Delete
-export async function deleteById(id){
-  const sqlString = `DELETE FROM joinclean WHERE id=$1 RETURNING *;`
-  const result = await query(sqlString,[id]);
-  return result
+export async function deleteById(id) {
+  const sqlString = `DELETE FROM joinclean WHERE id=$1 RETURNING *;`;
+  const result = await query(sqlString, [id]);
+  return result;
 }
 
-
-      
+//adding a random comment
